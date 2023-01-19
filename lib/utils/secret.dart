87b5fece36b71ac5cea8bd80eac7f582ff85db4e
Amit Amrutiya <91112485+amitamrutiya2210@users.noreply.github.com>
2023-01-19 23:40:34 +0530

    create file

---
+++ lib/utils/secret.dart
@@ -0,0 +1,18 @@
+class SECRET {
+  
+  // use email.js for service,template and user id
+  static String CONTACT_US_SERVICE_ID = 'SERVICE_ID';
+  static String CONTACT_US_TEMPLATE_ID = 'TEMPLATE_ID';
+  static String CONTACT_US_USER_ID = 'USER_ID';
+
+  static String DONATION_SERVICE_ID = 'SERVICE_ID';
+  static String DONATION_TEMPLATE_ID = 'TEMPLATE_ID';
+  static String DONATION_USER_ID = 'USER_ID';
+
+  static String SLOT_BOOKING_SERVICE_ID = 'SERVICE_ID';
+  static String SLOT_BOOKING_TEMPLATE_ID = 'TEMPLATE_ID';
+  static String SLOT_BOOKING_USER_ID = 'USER_ID';
+  
+  //create razorpay account and add api key
+  static String RAZORPAY_SECRED_ID = 'RAZORPAY_ID';
+}
